93f75d4db11d0e2c34cf1fe3439237f3cfd9700c
ottoste <ottotahtracing@gmail.com>
2015-10-13 16:43:00 +0100

    Tests pass

--- lib/maxi.ex
+++ lib/maxi.ex
@@ -1,2 +1,10 @@
 defmodule Maxi do
+	def max(a , b) do
+		if a > b do
+		 a
+		else
+			b
+		end
+
+	end
 end
--- test/maxi_test.exs
+++ test/maxi_test.exs
@@ -2,7 +2,13 @@ defmodule MaxiTest do
   use ExUnit.Case
   doctest Maxi
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+  test "given 1 and 2, the answer should be 2" do
+  	assert Maxi.max(1, 2) == 2
   end
+
+  test "given 5 and 3, the answer should be 5" do
+  	assert Maxi.max(5, 3) == 5
+  end
+
 end
